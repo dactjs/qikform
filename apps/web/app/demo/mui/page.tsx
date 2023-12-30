3dede4cede69661508cd3fb72318de2b8ca37dcf
Diego Cruz <dcruz@indocal.gob.do>
2023-12-30 16:28:32 -0400

    fix: user dynamic because mui-tiptap uses window object

--- apps/web/app/demo/mui/page.tsx
+++ apps/web/app/demo/mui/page.tsx
@@ -1,20 +1,28 @@
 "use client";
 
+import dynamic from "next/dynamic";
 import { Box } from "@mui/material";
 
 import { FormSchema } from "@qikform/core";
-import { FormBuilder } from "@qikform/mui";
 
 import { ToggleColorModeButton } from "~/theme";
 
 import { schema } from "./_schema";
+
+const DynamicFormBuilder = dynamic(
+  () => import("@qikform/mui").then(({ FormBuilder }) => FormBuilder),
+  { ssr: false } // No ssr for this component (it uses the window object)
+);
 
 export default function MuiDemoPage(): React.ReactElement {
   const form = FormSchema.parse(schema);
 
   return (
     <Box sx={{ width: "100vw", height: "100vh", overflow: "hidden" }}>
-      <FormBuilder form={form} toolbarContent={<ToggleColorModeButton />} />
+      <DynamicFormBuilder
+        form={form}
+        toolbarContent={<ToggleColorModeButton />}
+      />
     </Box>
   );
 }
